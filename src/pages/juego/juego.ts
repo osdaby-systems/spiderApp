fc88d00507664c7458042dd513e231256121da1c
Darwin Guailla <darwinj94@hotmail.com>
2017-11-16 13:18:39 -0500

    paginas que faltaron

---
+++ src/pages/juego/juego.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-juego',
+  templateUrl: 'juego.html'
+})
+export class JuegoPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}

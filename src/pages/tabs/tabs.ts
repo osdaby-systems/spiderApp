4695e7d2f17a10387a1e181d8d976ed94e7eab93
Darwin Guailla <darwinj94@hotmail.com>
2017-11-16 01:10:35 -0500

    menu principal parcialmente terminado

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
+import { JuegoPage } from '../juego/juego';
+import { TestPage } from '../test/test';
 import { HomePage } from '../home/home';
 
 @Component({
@@ -10,8 +10,8 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab2Root = JuegoPage;
+  tab3Root = TestPage;
 
   constructor() {
 
